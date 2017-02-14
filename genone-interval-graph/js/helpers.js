e9f8743b76b6eea9b7a0601f17a1831c2ac06a26
xanthopoulakis <c.xanthopoulakis@gmail.com>
2017-02-15 00:20:58 +0100

    Add panels and legend with brush and zoom functionality

---
+++ genone-interval-graph/js/helpers.js
@@ -0,0 +1,34 @@
+// Function to flatten an array of arrays
+function flatten(ary) {
+  return ary.reduce(function(a, b) {
+    if (Array.isArray(b)) {
+      return a.concat(flatten(b))
+    }
+    return a.concat(b)
+  }, [])
+}
+
+// Function to group the input data by chromosome
+function getChromosomeBins(dataArray) {
+  var bins = d3.map([], function(d) { return d.chromosome; });;
+  dataArray.intervals.forEach(function(d,i) { 
+    if (!bins.has(d.chromosome)) {
+        bins.set(d.chromosome, {minPoint: undefined, maxPoint: undefined, minY: undefined, maxY: undefined, intervals: []});
+      }
+      var bin = bins.get(d.chromosome);
+      bin.chromosome = d.chromosome;
+      bin.minPoint = d3.min([d.startPoint, bin.minPoint]);
+      bin.maxPoint = d3.max([d.endPoint, bin.maxPoint]);
+      bin.minY = d3.min([d.y, bin.minY]);
+      bin.maxY = d3.max([d.y, bin.maxY]);
+      bin.intervals.push(d);
+      bins.set(d.chromosome, bin);
+  });
+  return bins;
+}
+
+function getChromosomesExtent(bins) {
+  var minPoint = d3.min(bins.values().map(function(d,i) { return d.minPoint}));
+  var maxPoint = d3.max(bins.values().map(function(d,i) { return d.maxPoint}));
+  return [minPoint, maxPoint];
+}
