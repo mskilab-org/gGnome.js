2a720f7ad4e47b4b600ebbd601f5341ad3ec11e1
xanthopoulakis <c.xanthopoulakis@gmail.com>
2017-03-14 20:54:38 +0100

    Fix bug on the sourceless/sinkless edges represented on the 3 different panels

--- genone-interval-graph/js/helpers.js
+++ genone-interval-graph/js/helpers.js
@@ -13,8 +13,8 @@ function getConnectionBins(dataArray, intervalBins) {
   return dataArray.connections.reduce(function(hash, elem) { 
     hash[elem.cid] = {
       connection: elem,
-      source: intervalBins[Math.abs(elem.source)],
-      sink: intervalBins[Math.abs(elem.sink)]
+      source: intervalBins[elem.source && Math.abs(elem.source)],
+      sink: intervalBins[elem.sink && Math.abs(elem.sink)]
     }; 
     return hash; }, {});
 }
@@ -23,7 +23,7 @@ function getLocalConnectionBins(dataArray, connectionBins) {
   return dataArray.connections.reduce(function(hash, elem) {
   var source = connectionBins[elem.cid].source;
   var sink = connectionBins[elem.cid].sink;
-  if (source.chromosome === sink.chromosome) {
+  if ((elem.type !== 'UKN') && (source.chromosome === sink.chromosome)) {
     if (!hash[source.chromosome]) {
       hash[source.chromosome] = [];
     }
@@ -37,8 +37,27 @@ function getInterChromosomeConnectionBins(dataArray, panels, connectionBins) {
   return dataArray.connections.filter(function(elem, index) {
     var source = connectionBins[elem.cid].source;
     var sink = connectionBins[elem.cid].sink;
-    return ((source.chromosome != sink.chromosome) && (panelChromosomes.includes(source.chromosome)) && (panelChromosomes.includes(sink.chromosome)));
+    return ((elem.type !== 'UKN') && (source.chromosome != sink.chromosome) && (panelChromosomes.includes(source.chromosome)) && (panelChromosomes.includes(sink.chromosome)));
   });
+}
+// The hashmap mapping the connection id to the unknown connections within the same chromosome
+function getUknownConnectionBins(dataArray, connectionBins) {
+  return dataArray.connections.reduce(function(hash, elem) {
+  var source = connectionBins[elem.cid].source;
+  var sink = connectionBins[elem.cid].sink;
+  if (!source && sink) {
+    if (!hash[sink.chromosome]) {
+      hash[sink.chromosome] = [];
+    }
+    hash[sink.chromosome].push(elem);
+  }
+  if (source && !sink) {
+    if (!hash[source.chromosome]) {
+      hash[source.chromosome] = [];
+    }
+    hash[source.chromosome].push(elem);
+  }
+  return hash; }, {});
 }
 // The title for the popover on the intervals
 function popoverIntervalTitle(d,i) {
@@ -63,9 +82,18 @@ function popoverConnectionTitle(d,i) {
 // The content for the popover on the connections
 function popoverConnectionContent(d,i) {
   var content = '';
-  [{label: 'Type', value: d.type}, {label: 'Source Chromosome', value: d.sourceChromosome}, {label: 'Source Interval', value: Math.abs(d.source) + (d.source > 0 ? ' (tail)' : ' (head)')}, {label: 'Source Point', value: d3.format(',')(d.sourcePoint)},
+  var array = [{label: 'Type', value: d.type}, {label: 'Source Chromosome', value: d.sourceChromosome}, {label: 'Source Interval', value: Math.abs(d.source) + (d.source > 0 ? ' (tail)' : ' (head)')}, {label: 'Source Point', value: d3.format(',')(d.sourcePoint)},
    {label: 'Source Jabba', value: (d.sourceJabba)}, {label: 'Sink Chromosome', value: d.sinkChromosome}, {label: 'Sink Interval', value: Math.abs(d.sink) + (d.sink > 0 ? ' (tail)' : ' (head)')},
-   {label: 'Sink Point', value: d3.format(',')(d.sinkPoint)}, {label: 'Sink Jabba', value: (d.sinkJabba)}, {label: 'Distance', value: d3.format(',')(d.distance)}].forEach(function(e,j) {
+   {label: 'Sink Point', value: d3.format(',')(d.sinkPoint)}, {label: 'Sink Jabba', value: (d.sinkJabba)}, {label: 'Distance', value: (d.distance ? d3.format(',')(d.distance) : '-')}];
+  if (!d.source) {
+    array = [{label: 'Sink Chromosome', value: d.sinkChromosome}, {label: 'Sink Interval', value: Math.abs(d.sink) + (d.sink > 0 ? ' (tail)' : ' (head)')},
+    {label: 'Sink Point', value: d3.format(',')(d.sinkPoint)}, {label: 'Sink Jabba', value: (d.sinkJabba)}, {label: 'Distance', value: (d.distance ? d3.format(',')(d.distance) : '-')}];
+  }
+  if (!d.sink) {
+    array = [{label: 'Type', value: d.type}, {label: 'Source Chromosome', value: d.sourceChromosome}, {label: 'Source Interval', value: Math.abs(d.source) + (d.source > 0 ? ' (tail)' : ' (head)')}, {label: 'Source Point', value: d3.format(',')(d.sourcePoint)},
+    {label: 'Source Jabba', value: (d.sourceJabba)}, {label: 'Distance', value: (d.distance ? d3.format(',')(d.distance) : '-')}];
+  }
+  array.forEach(function(e,j) {
      content += '<tr><td class="table-label" align="left" width="200" valign="top"><strong>' + e.label + ':</strong></td><td class="table-value" width="100" align="right" valign="top">' + e.value + '</td></tr>';
    });
   return '<div class="row"><div class="col-lg-12"><table width="0" border="0" align="left" cellpadding="0" cellspacing="0"><tbody>' + content + '</tbody></table></div></div>';
@@ -177,4 +205,31 @@ function calculateInterConnectorEndpoints(yScale, record, connector, panelsArray
         [sinkChromosome.panelScale(connector.sink.endPoint), yScale(connector.sink.y)]];
     }
   }
+}
+
+// The array of points forming the unknown connections with one endpoint missing
+function calculateUknownConnectorEndpoints(yScale, record, connector, chromosome) {
+  if (!record.source && !record.sink) return;
+  var touchpointX, touchpointY, touchpointSign;
+  if (record.source && !record.sink) {
+    record.sourceJabba = connector.source.y;
+    record.sourceChromosome = connector.source.chromosome;
+    record.sourcePoint = connector.connection.source > 0 ? connector.source.endPoint : connector.source.startPoint;
+    touchpointX = record.sourcePoint;
+    touchpointY = connector.source.y;
+    touchpointSign = Math.sign(connector.connection.source);
+  }
+  if (!record.source && record.sink) {
+    record.sinkJabba = connector.sink.y;
+    record.sinkChromosome = connector.sink.chromosome;
+    record.sinkPoint = connector.connection.sink > 0 ? connector.sink.endPoint : connector.sink.startPoint;
+    touchpointX = record.sinkPoint;
+    touchpointY = connector.sink.y;
+    touchpointSign = Math.sign(connector.connection.sink);
+  }
+  return [
+    [chromosome.scale(touchpointX), yScale(touchpointY)],
+    [chromosome.scale(touchpointX) + touchpointSign * 20, yScale(touchpointY + 0.17)],
+    [chromosome.scale(touchpointX) - touchpointSign * 20, yScale(touchpointY + 0.34)],
+    [chromosome.scale(touchpointX), yScale(touchpointY + 0.5)]];
 }
