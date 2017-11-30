3c509bc0c0f1092006202bda1f1c8a2109fba006
C. Xanthopoulakis <c.xanthopoulakis@gmail.com>
2017-11-30 19:58:43 +0100

    change tail to right and head to left

--- complete-genome-interval-graph/js/connection.js
+++ complete-genome-interval-graph/js/connection.js
@@ -138,7 +138,7 @@ class Connection extends Base {
     var array = [
       ['&nbsp;', '<strong>Source</strong>', '<strong>Sink</strong>'], 
       ['Chromosome', ((!this.source) ? 'Unknown' : this.source.interval.chromosome), ((!this.sink) ? 'Unknown' : this.sink.interval.chromosome)], 
-      ['Interval', ((!this.source) ? 'Unknown' : (this.source.intervalId + (this.source.sign > 0 ? ' (tail)' : ' (head)'))), ((!this.sink) ? 'Unknown' : (this.sink.intervalId + (this.sink.sign > 0 ? ' (tail)' : ' (head)')))],
+      ['Interval', ((!this.source) ? 'Unknown' : (this.source.intervalId + (this.source.sign > 0 ? ' (right)' : ' (left)'))), ((!this.sink) ? 'Unknown' : (this.sink.intervalId + (this.sink.sign > 0 ? ' (right)' : ' (left)')))],
       ['Point (chromosome)', ((!this.source) ? 'Unknown' : d3.format(',')(this.source.point)), ((!this.sink) ? 'Unknown' : d3.format(',')(this.sink.point))],
       ['Point (genome)', ((!this.source) ? 'Unknown' : d3.format(',')(this.source.place)), ((!this.sink) ? 'Unknown' : d3.format(',')(this.sink.place))],
       ['Jabba', ((!this.source) ? 'Unknown' : (this.source.y)), ((!this.sink) ? 'Unknown' : (this.sink.y))]
