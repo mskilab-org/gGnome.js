bc1bf2b3314cb74d42fafef24c43be1e137ac23a
Evan Biederstedt <evan.biederstedt@gmail.com>
2018-04-20 16:48:56 -0400

    finished up combinationsTest.js

--- test/chromoTest.js
+++ test/chromoTest.js
@@ -8,4 +8,13 @@ var chromo = require('../js/chromo.js');
 
 
 // var Chromo = new chromo.Chromo('chr2', 57000, 65000, 'blue');
-var Chromo = new chromo.Chromo(2, 57000, 65000, 'blue');
+
+var Chromo = new chromo.Chromo(chromosome = '2', startPoint = 57000, endPoint = 65000, color = 'blue', scale = 10000 );
+
+
+// console.log(Chromo.chromosome)
+
+// console.log(Chromo.contains(100))
+
+// console.log(Chromo.chromoWidth())
+
--- test/combinationsTest.js
+++ test/combinationsTest.js
@@ -44,37 +44,20 @@ describe('testing combinations.js', function() {
 });
 
 
-console.log(combinations.k_combinations([1, 2, 3], 2)[0])
-console.log(typeof(combinations.k_combinations([1, 2, 3], 2)[0]))
-
-console.log(combinations.k_combinations([1, 2, 3], 2)[0] instanceof Array)
-
-console.log(Array(1, 2))
-console.log(typeof(Array(1, 2)))
-
-console.log(Array(1, 2) instanceof Array)
-
-
-
 describe('testing combinations.js', function() {
-  it('k_combinations(set, k), get elements', function() {
+  it('k_combinations(set, k), check output elements', function() {
     assert.equal(combinations.k_combinations([1, 2, 3], 1)[0], 1);
-    //assert.equal(combinations.k_combinations([1, 2, 3], 2)[0], Array(1, 2));
-    // assert.(combinations.k_combinations([1, 2, 3], 3)[0], Array(1, 2, 3));
-    //assert.equal(combinations.k_combinations([1, 2, 3], 4).length, 0);
-    //assert.equal(combinations.k_combinations([1, 2, 3], 0).length, 0);
-    //assert.equal(combinations.k_combinations([1, 2, 3], -1).length, 0);
-    //assert.equal(combinations.k_combinations([], 0).length, 0);    
+    expect(combinations.k_combinations([1, 2, 3], 2)[0]).to.eql([1, 2]).but.not.equal([1, 2]);
+    expect(combinations.k_combinations([1, 2, 3], 3)[0]).to.eql([1, 2, 3]).but.not.equal([1, 2, 3]);
+    assert.equal(combinations.k_combinations([1, 2, 3], 4).length, 0);
+    assert.equal(combinations.k_combinations([1, 2, 3], 0).length, 0);
+    assert.equal(combinations.k_combinations([1, 2, 3], -1).length, 0);
+    assert.equal(combinations.k_combinations([], 0).length, 0);    
   });
 });
 
 
 
- // combinations([1, 2, 3])
- // -> [[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3]]
- // 
- // combinations([1])
- // -> [[1]]
 
 describe('testing combinations.js', function() {
   it('combinations(set), checking output lengths', function() {
@@ -85,8 +68,10 @@ describe('testing combinations.js', function() {
 
 
 describe('testing combinations.js', function() {
-  it('combinations(set), checking output lengths', function() {
+  it('combinations(set), checking output elements', function() {
     assert.equal(combinations.combinations([1, 2, 3])[0], 1);
+    expect(combinations.combinations([1, 2, 3])[3]).to.eql([1, 2]).but.not.equal([1, 2]);
+    expect(combinations.combinations([1, 2, 3])[6]).to.eql([1, 2, 3]).but.not.equal([1, 2, 3]);
     assert.equal(combinations.combinations([1])[0], 1);   
   });
 });
