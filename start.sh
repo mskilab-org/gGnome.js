4382d62e8f4d3a397bbd7692d038ac2e90e4e185
ShaiberAlon <alon.shaiber@gmail.com>
2021-11-23 15:13:24 -0500

    removed git-lfs dependency. Added instructions for downloading JSON files from AWS. Added metadata files for hg19_chr and hg38 with and without chr prefix.

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+if [ ! -s public/genes.json ]; then
+    echo 'Downloading genes.json (hg19)'
+    wget -P public/ https://mskilab.s3.amazonaws.com/pgv/genes.json
+fi
 open http://localhost:8080/index.html
 npm start
