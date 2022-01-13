24204e6a6b5d9372c4c2df16a92578c52c186991
Alon Shaiber <ShaiberAlon@users.noreply.github.com>
2022-01-13 16:51:47 -0500

    Updated start.sh: download hg19 genes.json
    The name of the file was updated on AWS so changing here to in accordance with that change.

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [ ! -s public/genes.json ]; then
     echo 'Downloading genes.json (hg19)'
-    wget -P public/ https://mskilab.s3.amazonaws.com/pgv/genes.json
+    wget -O public/genes.json https://mskilab.s3.amazonaws.com/pgv/hg19.genes.json
 fi
 open http://localhost:8080/index.html
 npm start
